f9c9fcfa4d04104f88d83e65fcfda88d0d645e5b
Craig Huckerby <craighckby@gmail.com>
2026-03-20 20:29:39 +1000

    Meta-123: Lifecycle Instantiation R6 for nexus_core.js | Vote: QISKIT/Qiskit

--- nexus_core.js
+++ nexus_core.js
@@ -1,117 +1,144 @@
 // nexus_core.js
 import { Observable, of, from, forkJoin, throwError } from 'rxjs';
-import { tap, catchError, mergeMap } from 'rxjs/operators';
+import { tap, catchError, mergeMap, shareReplay } from 'rxjs/operators';
 import { console, process } from 'node';
 
-class MultiStrategicFactory {
-  static defaultFactory = new StrategicFactory('default');
-
-  static async getStrategy(strategyFactoryName: string, context: any): Promise<Observable<any>> {
-    const strategyFactory = this.getFactory(strategyFactoryName);
-    if (strategyFactory) {
-      return from(strategyFactory(context));
-    }
-    throw new Error(`Strategy factory ${strategyFactoryName} not found`);
+class ModuleRegistry {
+  constructor() {
+    this.modules = {};
   }
 
-  static getFactory(strategyFactoryName: string): StrategicFactory {
-    if (strategyFactoryName === 'default') {
-      return this.defaultFactory;
-    }
-    if (require('qiskit')?.moduleRegistryModule) {
-      const { moduleRegistryModule } = require('qiskit');
-      const strategyFactories = moduleRegistryModule.getFactories();
-      for (const factory of strategyFactories) {
-        if (factory.name === strategyFactoryName) {
-          MultiStrategicFactory.registerFactory(factory);
-          return factory;
-        }
-      }
-    }
-    throw new Error(`Strategy factory ${strategyFactoryName} not found`);
+  registerModule(moduleName, module) {
+    this.modules[moduleName] = module;
   }
 
-  static registerFactory(strategyFactory: StrategicFactory): void {
-    switch (strategyFactory.name) {
-      case 'default':
-        break;
-      default:
-        console.log(`Registering new strategy factory: ${strategyFactory.name}`);
-    }
+  getModule(moduleName) {
+    return this.modules[moduleName] || null;
+  }
+
+  getModules() {
+    return Object.values(this.modules);
+  }
+
+  async loadModules() {
+    return Promise.all(this.getModules().map((module) => module.load()));
   }
 }
 
 class StrategicFactory {
-  constructor(private name: string) {}
+  constructor(name) {}
 
-  execute(context: any): Promise<any> {
-    throw new Error('Not implemented');
+  registerStrategy(strategy) {
+    this.strategies[name] = strategy;
+  }
+
+  getStrategy(strategyName) {
+    return this.strategies[strategyName];
+  }
+
+  getStrategies() {
+    return Object.values(this.strategies);
   }
 }
 
-class EnhancedStrategyRegistry {
-  #strategiesCache: { [key: string]: Strategy };
-
-  constructor(name: string) {
-    this.name = name;
-    this.#strategiesCache = {};
+class StrategyPlugin extends Observable {
+  constructor(strategyFactory, context) {
+    super((observer) => {
+      this.context = context;
+      this.strategyFactory = strategyFactory;
+      this.observer = observer;
+      this.strategyFactory.getStrategy(strategyName)
+      .subscribe({
+        next: (strategy) => {
+          this.observer.next(strategy.execute(this.context));
+        },
+        error: (error) => {
+          this.observer.error(error);
+        }
+      });
+    });
   }
 
-  async getStrategy(strategyName: string): Promise<Strategy> {
-    if (this.#strategiesCache[strategyName]) {
-      return this.#strategiesCache[strategyName];
-    }
-    try {
-      const strategy = await this.loadStrategy(strategyName);
-      this.#strategiesCache[strategyName] = strategy;
-      return strategy;
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Error loading strategy:', error);
-      } else {
-        console.error('Unknown error occurred:', error);
-      }
-      throw error;
-    }
-  }
-
-  private async loadStrategy(strategyName: string): Promise<Strategy> {
-    const { registryModule } = require('qiskit');
-    const strategyModule = registryModule.getModule(strategyName);
-    if (strategyModule) {
-      const strategy = registryModule.createStrategy(strategyModule, this.name);
-      return strategy;
-    }
-    throw new Error(`Strategy ${strategyName} not found in registry ${this.name}`);
-  }
-}
-
-class StrategyPlugin {
-  constructor(strategyFactory: StrategicFactory, private context: any) {}
-
-  async getStrategy(): Promise<Observable<any>> {
-    const strategy = await MultiStrategicFactory.getStrategy('default', this.context);
-    return from(strategy.execute(this.context));
+  static createPlugin(strategyFactory, context, strategyName) {
+    return new StrategyPlugin(strategyFactory, context);
   }
 }
 
 class EnhancedRegistryLoader {
-  constructor(private registryConfig: { [key: string]: any }) {}
-
-  async loadRegistry(registryName: string): Promise<EnhancedStrategyRegistry> {
-    const { registryConfigModule } = require('qiskit');
-    const registryConfig = await registryConfigModule.loadConfig(registryName);
-    if (!registryConfig) {
-      throw new Error(`Registry config for ${registryName} not found`);
-    }
-    const registry = new EnhancedStrategyRegistry(registryName, registryConfig);
-    await registry.initStrategies();
-    return registry;
+  constructor(registryConfig) {
+    this.registryConfig = registryConfig;
   }
 
-  private async initStrategies(): Promise<void> {
-    const { moduleRegistryModule } = require('qiskit');
-    const strategyModules = await moduleRegistryModule.getModules();
+  async loadRegistry(registryName) {
+    try {
+      const registryConfig = await require('qiskit').registryConfigModule.loadConfig(registryName);
+      if (!registryConfig) {
+        throw new Error(`Registry config for ${registryName} not found`);
+      }
+      const registry = new EnhancedStrategyRegistry(registryName, registryConfig);
+      await registry.initStrategies();
+      return registry;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async initStrategies(registry) {
+    const strategyModules = await require('qiskit').moduleRegistryModule.getModules();
+    for (const module of strategyModules) {
+      try {
+        await this.loadStrategyModule(registry, module.name);
+      } catch (error) {
+        console.error('Error loading strategy module:', error);
+      }
+    }
+  }
+
+  async loadStrategyModule(registry, moduleName) {
+    const module = await require('qiskit').registryModule.getModule(moduleName);
+    if (!module) {
+      throw new Error(`Module ${moduleName} not found`);
+    }
+    await module.register(registry.registryConfig);
+  }
+}
+
+class EnhancedStrategyRegistry {
+  constructor(name, registryConfig) {
+    this.name = name;
+    this.registryConfig = registryConfig;
+    this.strategies = new Map();
+  }
+
+  async loadStrategy(strategyName) {
+    try {
+      const strategyModule = require('qiskit').registryModule.getModule(strategyName);
+      if (strategyModule) {
+        const strategy = require('qiskit').registryModule.createStrategy(strategyModule, this.name);
+        await this.registerStrategy(strategy);
+        return strategy;
+      } else {
+        throw new Error(`Strategy ${strategyName} not found in registry ${this.name}`);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  registerStrategy(strategy) {
+    this.strategies.set(strategy.name, strategy);
+  }
+
+  getStrategy(strategyName) {
+    return this.strategies.get(strategyName);
+  }
+
+  async initStrategies() {
+    await this.loadStrategies();
+  }
+
+  async loadStrategies() {
+    const strategyModules = await require('qiskit').moduleRegistryModule.getModules();
     for (const module of strategyModules) {
       try {
         await this.loadStrategyModule(module.name);
@@ -121,63 +148,68 @@ class EnhancedRegistryLoader {
     }
   }
 
-  private async loadStrategyModule(moduleName: string): Promise<void> {
-    const { registryModule } = require('qiskit');
-    const module = await registryModule.getModule(moduleName);
+  async loadStrategyModule(moduleName) {
+    const module = await require('qiskit').registryModule.getModule(moduleName);
     if (!module) {
       throw new Error(`Module ${moduleName} not found`);
     }
     await module.register(this.registryConfig);
   }
+
+  static createRegistry(name, registryConfig) {
+    return new EnhancedStrategyRegistry(name, registryConfig);
+  }
 }
 
-class EnhancedExecutor {
-  async executeStrategies(strategyNames: string[], context: any): Promise<any> {
+class StrategicExecutor {
+  constructor(context) {
+    this.context = context;
+    this.strategicPlugins = [];
+
+    this.registryLoader = new EnhancedRegistryLoader({});
+  }
+
+  async loadRegistry(registryName) {
+    return this.registryLoader.loadRegistry(registryName);
+  }
+
+  async executeStrategies(strategyNames) {
     try {
-      const strategyPlugins: Promise<StrategyPlugin>[] = [];
+      const registryPromises = [];
       for (const strategyName of strategyNames) {
-        const registryLoader = new EnhancedRegistryLoader({});
-        const registry = await registryLoader.loadRegistry(strategyName);
-        const strategyPlugin = new StrategyPlugin(new StrategicFactory(''), context);
-        const strategy = await registry.getStrategy(strategyName);
-        strategyPlugin.strategy = strategy;
-        strategyPlugins.push(strategyPlugin.getStrategy());
+        const registry = await this.loadRegistry(strategyName);
+        const strategyFactory = registry.getStrategy(strategyName);
+        const strategyPlugin = await StrategyPlugin.createPlugin(strategyFactory, this.context, strategyName);
+        registryPromises.push(strategyPlugin);
       }
 
-      return forkJoin(strategyPlugins, (results) => results).pipe(
-        tap((result) => console.log(`Strategy execution result: ${result}`)),
+      const strategyResults = await forkJoin(registryPromises).pipe(
+        tap((registry) => console.log(`Strategy execution result: ${registry}`)),
         catchError((error) => {
           console.error('Error occurred:', error);
           return throwError(error);
         }),
-        mergeMap(async (result) => {
+        mergeMap(async (registry) => {
           const mergedResult = {};
-          for (const strategyName in result) {
-            mergedResult[strategyName] = await this.mergeResults(result[strategyName]);
+          for (const strategyName in registry.strategies) {
+            mergedResult[strategyName] = await this.mergeResults(registry.strategies[strategyName]);
           }
           return mergedResult;
-        })
-      );
+        }),
+        shareReplay(1)
+      ).toPromise();
+
+      return strategyResults;
     } catch (error) {
       console.error('Error occurred:', error);
     }
   }
 
-  private async mergeResults(results: any): Promise<any> {
-    return results;
+  async mergeResults(strategyResult) {
+    return strategyResult;
+  }
+
+  static createExecutor(context) {
+    return new StrategicExecutor(context);
   }
 }
-
-async function main(): Promise<void> {
-  try {
-    const context = { state: 'initial' };
-
-    const enhancedExecutor = new EnhancedExecutor();
-    const strategyNames = ['example-strategy'];
-    await enhancedExecutor.executeStrategies(strategyNames, context);
-  } catch (error) {
-    console.error('Error occurred:', error);
-  }
-}
-
-process.on('processStartup', async () => {});
